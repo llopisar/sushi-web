d274d36a026b4b34e35d3dd9d7a16b3f9de7f578
Augusto Llopis <augustollopisar@gmail.com>
2026-04-25 18:12:23 +0100

    sitemap add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://omakaseporto.com",
-  integrations: [tailwind({ applyBaseStyles: false })]
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    sitemap()
+  ]
 });
